0307c9422d0e1842e57e5bfad61c692b42c6b2f0
Winston Chen <Winston@Winstons-iMac.home>
2017-03-15 02:45:05 -0400

    worked on quickselect

--- Quickselect/QuickSelect.java
+++ Quickselect/QuickSelect.java
@@ -3,8 +3,23 @@ public class QuickSelect{
     public QuickSelect(){};
 
     public static int part(int[] data, int start, int end){
-	pivot = data[Math.random() * data.length / 1];
-	int[
+	int pivot = data[Math.random() * data.length / 1];
+	ArrayList<int> a1 = new ArrayList<int>();
+	for(int i = 0; i < data.length; i++){
+	    if(data[i] < pivot){
+		a1.add(data[i]);
+	    }
+	}
+	for(int i = 0; i < data.length; i++){
+	    if(data[i] ==  pivot){
+		a1.add(data[i]);
+	    }
+	}
+	for(int i = 0; i < data.length; i++){
+	    if(data[i] == pivot){
+		a1.add(data[i]);
+	    }
+	}
+	return a1.indexOf(pivot);
     }
-    
 }
