52e1a47531e1d9fc4ca3f1f1f9aa47d8ec0cfe42
Winston <winstonchen88899@gmail.com>
2017-03-24 11:47:40 -0400

    started merge

---
+++ 08Merge/Merge.java
@@ -0,0 +1,16 @@
+public class Merge{
+
+    public Merge(){};
+
+    public static void mergesort(int[] ary){
+	int[] a = copyOfRange(
+	
+	merge(ary, 0, h);
+    }
+
+    public static void merge(int[] a, int[] b, int[] destination){
+	
+
+    }
+
+}
